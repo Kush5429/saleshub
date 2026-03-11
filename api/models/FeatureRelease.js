b53947566b975b41c0bb128cff22420290ebc63c
Kush Ambekar <123255744+Kush5429@users.noreply.github.com>
2026-03-12 02:17:39 +0530

    Delete api/models/FeatureRelease.js

--- api/models/FeatureRelease.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const s = new mongoose.Schema({
-  featureName:  { type: String, required: true, trim: true },
-  description:  { type: String, default: "" },
-  releaseMonth: { type: String, default: "" },
-  useCase:      { type: String, default: "" },
-  demoLink:     { type: String, default: "" },
-}, { timestamps: true });
-module.exports = mongoose.models.FeatureRelease || mongoose.model("FeatureRelease", s);
