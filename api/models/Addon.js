eb2ad547af97190cda90797eff269ca13398990a
Kush Ambekar <123255744+Kush5429@users.noreply.github.com>
2026-03-12 02:17:13 +0530

    Delete api/models/Addon.js

--- api/models/Addon.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const s = new mongoose.Schema({
-  name:            { type: String, required: true, trim: true },
-  description:     { type: String, default: "" },
-  price:           { type: String, required: true },
-  compatiblePlans: { type: String, default: "" },
-}, { timestamps: true });
-module.exports = mongoose.models.Addon || mongoose.model("Addon", s);
